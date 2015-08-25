52e1652deea311cd45822460c87b0785f0479d75
Jeff Byrnes <jeff@evertrue.com>
2015-08-25 13:19:01 -0400

    Specify platform & version in ChefSpec
    No real tests yet, but still, this at least lets it run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'chefspec/berkshelf'
 RSpec.configure do |config|
   config.formatter = :documentation
   config.color = true
+  config.platform = 'ubuntu'
+  config.version = '14.04'
 end
 
 at_exit { ChefSpec::Coverage.report! }
