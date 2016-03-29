1323fd7570bdc904fa53dd435199e163bbe0e1de
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-03-29 15:22:37 -0400

    Use released version of hashicorp-vault ~> 2.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-set['vault']['version'] = '0.2.0'
-set['vault']['config']['address'] = '0.0.0.0:8200'
-set['vault']['config']['tls_disable'] = 'true'
-set['vault']['config']['backend_type'] = 'zookeeper'
-set['vault']['manage_certificate'] = false
+set['hashicorp-vault']['version'] = '0.2.0'
+set['hashicorp-vault']['config']['address'] = '0.0.0.0:8200'
+set['hashicorp-vault']['config']['tls_disable'] = 'true'
+set['hashicorp-vault']['config']['backend_type'] = 'zookeeper'
+set['hashicorp-vault']['manage_certificate'] = false
