cddb6380be21569aaf98ca66002e315a3564dff9
Eric Herot <eric.github@herot.com>
2016-06-06 14:27:21 -0400

    Also replace the stage worker token

--- recipes/replace_vault_token.rb
+++ recipes/replace_vault_token.rb
@@ -1,12 +1,27 @@
-replace_vault_token 'default' do
-  vault_host "http://vault.service.#{node.chef_environment}-us-east-1.consul:8200"
+replace_vault_token 'prod default' do
+  token_name 'default'
+  vault_host 'http://vault.service.prod-us-east-1.consul:8200'
   data_bag_name 'vault'
   data_bag_item_name 'tokens'
   options(
     ttl: '8670h',
     policies: %w(default apps)
   )
-  top_level_key node.chef_environment
+  top_level_key 'prod'
   min_remaining_ttl 3600
-  accessor_token data_bag_item('vault', 'tokens')[node.chef_environment]['vault']['worker_token']
+  accessor_token data_bag_item('vault', 'tokens')['prod']['vault']['worker_token']
+end
+
+replace_vault_token 'stage default' do
+  token_name 'default'
+  vault_host 'http://vault.service.stage-us-east-1.consul:8200'
+  data_bag_name 'vault'
+  data_bag_item_name 'tokens'
+  options(
+    ttl: '8670h',
+    policies: %w(default apps)
+  )
+  top_level_key 'stage'
+  min_remaining_ttl 3600
+  accessor_token data_bag_item('vault', 'tokens')['stage']['vault']['worker_token']
 end
