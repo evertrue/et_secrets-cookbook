282228b6c08798007b16e28a89902fa92325afa6
Eric Herot <eric.github@herot.com>
2016-06-06 16:00:21 -0400

    Make the replace_vault_token recipe DRYer

--- recipes/replace_vault_token.rb
+++ recipes/replace_vault_token.rb
@@ -1,27 +1,15 @@
-replace_vault_token 'prod default' do
-  token_name 'default'
-  vault_host 'http://vault.service.prod-us-east-1.consul:8200'
-  data_bag_name 'vault'
-  data_bag_item_name 'tokens'
-  options(
-    ttl: '8670h',
-    policies: %w(default apps)
-  )
-  top_level_key 'prod'
-  min_remaining_ttl 3600
-  accessor_token data_bag_item('vault', 'tokens')['prod']['vault']['worker_token']
-end
-
-replace_vault_token 'stage default' do
-  token_name 'default'
-  vault_host 'http://vault.service.stage-us-east-1.consul:8200'
-  data_bag_name 'vault'
-  data_bag_item_name 'tokens'
-  options(
-    ttl: '8670h',
-    policies: %w(default apps)
-  )
-  top_level_key 'stage'
-  min_remaining_ttl 3600
-  accessor_token data_bag_item('vault', 'tokens')['stage']['vault']['worker_token']
+%w(stage prod).each do |chef_env|
+  replace_vault_token "#{chef_env} default" do
+    token_name 'default'
+    vault_host "http://vault.service.#{chef_env}-us-east-1.consul:8200"
+    data_bag_name 'vault'
+    data_bag_item_name 'tokens'
+    options(
+      ttl: '8670h',
+      policies: %w(default apps)
+    )
+    top_level_key chef_env
+    min_remaining_ttl 3600
+    accessor_token data_bag_item('vault', 'tokens')[chef_env]['vault']['worker_token']
+  end
 end
