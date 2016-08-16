e578f06b6feb539615102b499da94b4f6d471d10
Eric Herot <eric.github@herot.com>
2016-08-16 14:37:49 -0400

    Set default lease ttl to 1 year

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 set['hashicorp-vault']['config']['address'] = '0.0.0.0:8200'
 set['hashicorp-vault']['config']['tls_disable'] = true
 set['hashicorp-vault']['config']['backend_type'] = 'consul'
-set['hashicorp-vault']['config']['default_lease_ttl'] = '720h' # 1 month
+set['hashicorp-vault']['config']['default_lease_ttl'] = '8760h' # 1 year
 set['hashicorp-vault']['config']['max_lease_ttl'] = '8760h' # 1 year
 set['hashicorp-vault']['manage_certificate'] = false
 
